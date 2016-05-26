ed301428f4256e33fe70ffb7872fec533dc9efb8
lukaschen <villagelukaschen@gmail.com>
2016-05-26 22:08:19 +0800

    update

--- _javascript/controller/mainController.js
+++ _javascript/controller/mainController.js
@@ -1,27 +1,8 @@
 angular.module('site')
-.controller('mainController',function($scope) {
+.controller('mainController',[ '$scope', '$mdSidenav', function($scope,$mdSidenav) {
     var vm = this;
 
-    vm.ribbon = [
-     	{
-     		name: "Off",
-     		color: "#DFE3E4",
-     		tooltip: "Development is currently happening off GitHub. It will be moved to V² GitHub soon"
-     	},
-     	{
-     		name: "Next",
-     		color: "#FF8000",
-     		tooltip: "This project is next in line to be moved to a V² GitHub repository"
-     	},
-     	{
-     		name: "Hot",
-     		color: "#da314b",
-     		tooltip: "We are moving this project right now to V² GitHub!"
-     	},
-     	{
-     		name: "On",
-     		color: "#6CC644",
-     		tooltip: "This project is on our V² GitHub"
-     	}
-     ];
-});
+    vm.toggleOffCanvas = function toggleOffCanvas(navID) {
+      $mdSidenav(navID).toggle();
+    }
+}]);
