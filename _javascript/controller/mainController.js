bd41c58886114a1d8f7ab7afa097cc2b2eccf3aa
lukaschen <villagelukaschen@gmail.com>
2016-05-28 23:34:35 +0800

    added fab menu

--- _javascript/controller/mainController.js
+++ _javascript/controller/mainController.js
@@ -2,7 +2,7 @@ angular.module('site')
 .controller('mainController',[ '$scope', '$mdSidenav', function($scope,$mdSidenav) {
     var vm = this;
 
-    vm.toggleOffCanvas = function toggleOffCanvas(navID) {
-      $mdSidenav(navID).toggle();
-    }
+    vm.openMenu = function($mdOpenMenu, ev) {
+      $mdOpenMenu(ev);
+    };
 }]);
